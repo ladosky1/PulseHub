d271af30e3c493b345e30b3c3cdfc520331d9023
oladojabasit29@gmail.com <oladojabasit29@gmail.com>
2026-09-06 12:18:01 +0100

    Added production frontend CORS origin

--- server/src/config/cors.ts
+++ server/src/config/cors.ts
@@ -1,10 +1,11 @@
 export const allowedOrigins = [
-    "http://localhost:5173", 
+    "http://localhost:5173",
     "http://127.0.0.1:5500",
-    "http://localhost:5500"
+    "http://localhost:5500",
+    "https://pulsehub-app.netlify.app",
 ];
 
 export const corsOptions = {
     origin: allowedOrigins,
     credentials: true,
-}
+};
