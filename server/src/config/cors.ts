ebead294bbf974d39dfa1d9e9718e3d4aae60b17
oladojabasit29@gmail.com <oladojabasit29@gmail.com>
2026-09-08 08:18:41 +0100

    Added the updated netlify dns to the backend cors

--- server/src/config/cors.ts
+++ server/src/config/cors.ts
@@ -3,6 +3,7 @@ export const allowedOrigins = [
     "http://127.0.0.1:5500",
     "http://localhost:5500",
     "https://pulsehub-app.netlify.app",
+    "https://pulsehub.com.ng"
 ];
 
 export const corsOptions = {
